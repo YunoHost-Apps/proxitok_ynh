87f73ccc5ab4437207f9d6c2169057613061b56c
ericgaspar <junk.eg@free.fr>
2022-03-29 23:39:09 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,7 +10,7 @@ YNH_PHP_VERSION="8.0"
 YNH_COMPOSER_VERSION="2.2.9"
 
 # dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl"
+pkg_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl"
 
 #=================================================
 # PERSONAL HELPERS
