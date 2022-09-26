43fb14d0f74bde34154ec366e972e1c598ef07c5
ericgaspar <junk.eg@free.fr>
2022-09-26 08:40:17 +0200

    2.4.2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 YNH_PHP_VERSION="8.0"
 
 # Composer version
-YNH_COMPOSER_VERSION="2.3.10"
+YNH_COMPOSER_VERSION="2.4.2"
 
 # dependencies used by the app
 pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl"
